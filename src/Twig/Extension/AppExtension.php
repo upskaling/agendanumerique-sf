ae1075fa1e9546e509951d8350069c46fa3f1105
upskaling <upskaling@users.noreply.github.com>
2024-04-04 13:33:35 +0200

    feat: ajout de jsonld

---
+++ src/Twig/Extension/AppExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Extension;
+
+use App\Twig\Runtime\AppJsonLdRuntime;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
+            // new TwigFilter('encodeJsonLd', [AppRuntime::class, 'doSomething']),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('encodeJsonLd', [AppJsonLdRuntime::class, 'encodeJsonLd'])
+        ];
+    }
+}
---
+++ src/Twig/Runtime/AppJsonLdRuntime.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Runtime;
+
+use App\Entity\Event;
+use Twig\Extension\RuntimeExtensionInterface;
+
+class AppJsonLdRuntime implements RuntimeExtensionInterface
+{
+    public function __construct()
+    {
+        // Inject dependencies if needed
+    }
+
+    public function encodeJsonLd($event)
+    {
+        if (!$event instanceof Event) {
+            throw new \InvalidArgumentException(sprintf('Expected an instance of %s, got %s', Event::class, get_class($event)));
+        }
+
+        return json_encode([
+            "@context" => "https://schema.org",
+            "@type" => "Event",
+            "name" => $event->getTitle(),
+            "description" => $event->getDescription(),
+            "startDate" => $event->getStartAt()->format('Y-m-d H:i:s'),
+            // "location" => [
+            //     "@type" => "Place",
+            //     "name" => "",
+            //     "address" => [
+            //         "@type" => "PostalAddress",
+            //         "addressLocality" => "",
+            //         "streetAddress" => ""
+            //     ]
+            // ],
+            "image" => $event->getImage(),
+            "organizer" => $event->getOrganizer(),
+            "url" => $event->getLink(),
+        ]);
+    }
+}
