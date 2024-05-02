68b414d9e7702ccaeab25f7cefeaf6eb916c676c
upskaling <upskaling@users.noreply.github.com>
2024-05-02 13:59:38 +0200

    feat: ajouter un bouton pour télécharger l'événement au format ics

--- src/Controller/ShowIcsController.php
+++ src/Controller/ShowIcsController.php
@@ -4,61 +4,41 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Event;
 use App\Repository\EventRepository;
-use Jsvrcek\ICS\CalendarExport;
-use Jsvrcek\ICS\CalendarStream;
-use Jsvrcek\ICS\Model\Calendar;
-use Jsvrcek\ICS\Model\CalendarEvent;
-use Jsvrcek\ICS\Utility\Formatter;
+use App\Utils\IcsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ShowIcsController extends AbstractController
 {
+    #[Route('/event/{id}/ics', name: 'app_event_show_ics', methods: ['GET'], requirements: ['id' => '\d+'])]
+    #[Route('/event/{slug}/ics', name: 'app_event_show_slug_ics', methods: ['GET'])]
+    public function showIcs(
+        Event $event,
+        IcsService $icsService,
+    ): Response {
+        $calendarExport = $icsService->getCalendar([$event]);
+
+        return new Response(
+            $calendarExport->getStream(),
+            Response::HTTP_OK,
+            [
+                'content-type' => 'text/calendar',
+                'content-disposition' => 'attachment; filename="'.$event->getSlug().'.ics"',
+            ]
+        );
+    }
+
     #[Route('/event.ics', name: 'app_event_ics', methods: ['GET'])]
     public function eventIcs(
         EventRepository $eventRepository,
+        IcsService $icsService
     ): Response {
-        $calendar = new Calendar();
-
-        foreach ($eventRepository->findLatest() as $event) {
-            $calendarEvent = new CalendarEvent();
-
-            $title = $event->getTitle();
-            if (null !== $title) {
-                $calendarEvent->setSummary($title);
-            }
-
-            $description = $event->getDescription();
-            if (null !== $description) {
-                $calendarEvent->setDescription(strip_tags($description));
-            }
-
-            $url = $event->getLink();
-            if (null !== $url) {
-                $calendarEvent->setUrl($url);
-            }
-
-            $uuid = $event->getUuid();
-            if (null !== $uuid) {
-                $calendarEvent->setUid($uuid->toRfc4122());
-            }
-
-            $startAt = $event->getStartAt();
-            if (null !== $startAt) {
-                $calendarEvent->setStart(\DateTime::createFromImmutable($startAt));
-            }
-
-            if (null !== $event->getEndAt()) {
-                $calendarEvent->setEnd(\DateTime::createFromImmutable($event->getEndAt()));
-            }
-
-            $calendar->addEvent($calendarEvent);
-        }
-
-        $calendarExport = (new CalendarExport(new CalendarStream(), new Formatter()))
-            ->addCalendar($calendar);
+        $calendarExport = $icsService->getCalendar(
+            $eventRepository->findLatest()
+        );
 
         return new Response(
             $calendarExport->getStream(),
---
+++ src/Utils/IcsService.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Utils;
+
+use App\Entity\Event;
+use Jsvrcek\ICS\CalendarExport;
+use Jsvrcek\ICS\CalendarStream;
+use Jsvrcek\ICS\Model\Calendar;
+use Jsvrcek\ICS\Model\CalendarEvent;
+use Jsvrcek\ICS\Utility\Formatter;
+
+class IcsService
+{
+    /**
+     * @param Event[] $events
+     */
+    public function getCalendar(array $events): CalendarExport
+    {
+        $calendar = new Calendar();
+
+        foreach ($events as $event) {
+            $calendarEvent = new CalendarEvent();
+
+            $title = $event->getTitle();
+            if (null !== $title) {
+                $calendarEvent->setSummary($title);
+            }
+
+            $description = $event->getDescription();
+            if (null !== $description) {
+                $calendarEvent->setDescription(strip_tags($description));
+            }
+
+            $url = $event->getLink();
+            if (null !== $url) {
+                $calendarEvent->setUrl($url);
+            }
+
+            $uuid = $event->getUuid();
+            if (null !== $uuid) {
+                $calendarEvent->setUid($uuid->toRfc4122());
+            }
+
+            $startAt = $event->getStartAt();
+            if (null !== $startAt) {
+                $calendarEvent->setStart(\DateTime::createFromImmutable($startAt));
+            }
+
+            if (null !== $event->getEndAt()) {
+                $calendarEvent->setEnd(\DateTime::createFromImmutable($event->getEndAt()));
+            }
+
+            $calendar->addEvent($calendarEvent);
+        }
+
+        return (new CalendarExport(new CalendarStream(), new Formatter()))
+            ->addCalendar($calendar);
+    }
+}
