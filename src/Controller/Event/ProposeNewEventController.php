94a838fbb903aa0f13b50a8e4126863f5a7ec403
upskaling <upskaling@users.noreply.github.com>
2024-10-26 12:37:25 +0200

    feat: ajout de l'envoi d'email de notification lors de la proposition d'un nouvel événement

--- src/Controller/Event/ProposeNewEventController.php
+++ src/Controller/Event/ProposeNewEventController.php
@@ -7,17 +7,24 @@ namespace App\Controller\Event;
 use App\DTO\EventValidationDTO;
 use App\Form\ProposeNewType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
+#[Route('/event/propose-new', name: 'app_event_propose_new')]
 class ProposeNewEventController extends AbstractController
 {
-    #[Route('/event/propose-new', name: 'app_event_propose_new')]
-    public function index(
+    public function __invoke(
         Request $request,
         EntityManagerInterface $entityManager,
+        MailerInterface $mailer,
+        #[Autowire('%mailer.from_email%')] string $fromEmail,
+        #[Autowire('%mailer.admin_email%')] string $adminEmail,
     ): Response {
         $eventValidationDTO = new EventValidationDTO('propose-new');
 
@@ -30,6 +37,18 @@ class ProposeNewEventController extends AbstractController
             $entityManager->persist($event);
             $entityManager->flush();
             $this->addFlash('success', 'Votre événement a bien été proposé');
+
+            $message = (new NotificationEmail())
+                ->from($fromEmail)
+                ->to($adminEmail)
+                ->subject('Un nouvel événement a été proposé')
+                ->htmlTemplate('mail/event-propose-new.html.twig')
+                ->action('Voir le nouvel événement', $this->generateUrl('app_event_show_slug', ['slug' => $event->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL))
+                ->content('Bonjour,
+                Un nouvel événement a été proposé sur agendanumerique.fr')
+            ;
+
+            $mailer->send($message);
 
             return $this->redirectToRoute('app_index');
         }
---
+++ tests/ProposeNewEventTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Entity\Event;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProposeNewEventTest extends WebTestCase
+{
+    protected function tearDown(): void
+    {
+        $enytyManager = self::getContainer()->get('doctrine')->getManager();
+        $eventRepository = $enytyManager->getRepository(Event::class);
+        $event = $eventRepository->findOneBy(['slug' => '1-foo']);
+        if ($event) {
+            $enytyManager->remove($event);
+        }
+        $enytyManager->flush();
+        parent::tearDown();
+    }
+
+    public function testProposeNewEvent(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/event/propose-new');
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testProposeNewEventWithInvalidData(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/event/propose-new');
+        $client->submitForm('Enregistrer', [
+            'propose_new[title]' => 'foo',
+            'propose_new[link]' => 'foo',
+            'propose_new[description]' => 'foo',
+            'propose_new[startAt]' => '2021-01-01 11:00:00',
+            'propose_new[endAt]' => '2021-01-01 10:00:00',
+            'propose_new[organizer]' => '1',
+            'propose_new[image]' => 'foo',
+            'propose_new[location]' => '1',
+        ]);
+        $this->assertResponseStatusCodeSame(422);
+    }
+
+    public function testProposeNewEventWithValidData(): void
+    {
+        $client = static::createClient();
+        $client->enableProfiler();
+        $client->request('GET', '/event/propose-new');
+        $client->submitForm('Enregistrer', [
+            'propose_new[title]' => 'foo',
+            'propose_new[link]' => 'foo',
+            'propose_new[description]' => 'foo',
+            'propose_new[startAt]' => '2021-01-01 10:00:00',
+            'propose_new[endAt]' => '2021-01-01 11:00:00',
+            'propose_new[organizer]' => '1',
+            'propose_new[image]' => 'foo',
+            'propose_new[location]' => '1',
+        ]);
+
+        $this->assertResponseStatusCodeSame(302);
+    }
+}
