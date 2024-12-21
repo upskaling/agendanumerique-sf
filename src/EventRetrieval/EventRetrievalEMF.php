de7f882b243510669f954313318acbbee32cc878
upskaling <upskaling@users.noreply.github.com>
2024-12-21 14:53:26 +0100

    fix: corrige la récupération des événements EMF

--- src/EventRetrieval/EventRetrievalEMF.php
+++ src/EventRetrieval/EventRetrievalEMF.php
@@ -6,15 +6,16 @@ namespace App\EventRetrieval;
 
 use App\DTO\EventValidationDTO;
 use App\Repository\PostalAddressRepository;
+use IntlDateFormatter;
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\String\Slugger\AsciiSlugger;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class EventRetrievalEMF implements EventRetrievalInterface
 {
     private const URI = 'https://emf.fr';
     private const NAME = 'emf';
+    private const ORGANIZER = 'emf';
 
     public function __construct(
         private readonly HttpClientInterface $httpClient,
@@ -22,124 +23,104 @@ class EventRetrievalEMF implements EventRetrievalInterface
     ) {
     }
 
-    /**
-     * @return iterable<string>
-     */
-    public function getUrl(): iterable
-    {
-        yield $this::URI.'/event/une-ia-massiste-pour-creer-mon-site-web/';
-        yield $this::URI.'/event/creer-un-chatbot-avec-scratch/';
-        yield $this::URI.'/event/mon-premier-jeu-video-pac-man/';
-        yield $this::URI.'/event/mon-premier-jeu-video-la-bataille-des-planetes/';
-        yield $this::URI.'/event/brickanoid-gare-aux-briques/';
-        yield $this::URI.'/event/mon-premier-jeu-video-ninja-fruits/';
-        yield $this::URI.'/event/mon-premier-jeu-video-les-fous-du-volant/';
-        yield $this::URI.'/event/mon-premier-jeu-video-snake-le-serpent-qui-se-mord-la-queue/';
-        yield $this::URI.'/event/je-debute-avec-chatgpt/';
-    }
-
     public function retrieveEvents(): array
     {
-        $responses = [];
-        foreach ($this->getUrl() as $url) {
-            $responses[] = $this->httpClient->request(
-                'GET',
-                $url
-            );
-        }
-
-        $eventValidationDTOList = [];
-        foreach ($responses as $response) {
-            $eventValidationDTOList = array_merge($eventValidationDTOList, $this->loadEvent($response));
-        }
-
-        return $eventValidationDTOList;
-    }
-
-    private function convertDate(string $date): \DateTimeImmutable|false
-    {
-        $date = str_replace(
-            ['jan.', 'fév.', 'mars', 'avril', 'mai', 'juin', 'juil', 'août', 'sep', 'oct.', 'nov.', 'déc.'],
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-            $date
+        $response = $this->httpClient->request(
+            'GET',
+            self::URI.'/events/?s=&date=&posts_per_page=20&tax=discipline[numerique]'
         );
-
-        $date = explode(
-            ' -> ',
-            $date
-        );
-
-        return \DateTimeImmutable::createFromFormat(
-            'd m Y H \h i',
-            $date[0]
-        );
-    }
-
-    /**
-     * @return EventValidationDTO[]
-     */
-    private function loadEvent(ResponseInterface $response): array
-    {
-        $organizer = 'emf';
 
         $content = $response->getContent();
 
         $crawler = new Crawler($content);
 
-        // .ec3_iconlet.ec3_past
-        // cherche si le bloc date est grisé, si oui, date dépassée donc pas d'evenet à gérer
-        try {
-            $date = $crawler->filter('.ec3_iconlet.ec3_past')->text();
+        $response = $crawler->filter('.elementor-46496>.elementor-element> .elementor-element.elementor-element-c3baf3a.e-con-full.e-flex.e-con.e-child')
+            ->each(function (Crawler $crawler) {
+                return $this->loadEvent($crawler);
+            });
 
-            return [];
-        } catch (\InvalidArgumentException $e) {
+        return $response;
+    }
+
+    private function convertDate(string $date): \DateTimeImmutable|false
+    {
+        // Configurez l'analyseur de dates avec IntlDateFormatter
+        $formatter = new \IntlDateFormatter(
+            'fr_FR', // Langue et région
+            \IntlDateFormatter::FULL, // Format pour la date complète
+            \IntlDateFormatter::SHORT // Format pour l'heure
+        );
+
+        // Ajustez le format pour correspondre exactement à votre chaîne
+        $formatter->setPattern("EEEE d MMMM yyyy 'à' HH'h'mm");
+
+        $timestamp = $formatter->parse($date);
+
+        if (false === $timestamp) {
+            return false;
+        }
+
+        return (new \DateTimeImmutable())->setTimestamp($timestamp);
+    }
+
+    private function loadEvent(Crawler $crawler): ?EventValidationDTO
+    {
+        $url = $crawler->filter('a')->attr('href');
+
+        if (null === $url) {
+            return null;
+        }
+
+        $response = $this->httpClient->request('GET', $url);
+        $content = $response->getContent();
+        $crawler = new Crawler($content);
+
+        $jsonLD = $crawler->filter('script[type="application/ld+json"]')->text();
+
+        /** @var array<string, mixed>|null $data */
+        $data = json_decode($jsonLD, true);
+        /** @var array<string, mixed>|null $graph */
+        $graph = ((array) ($data['@graph'] ?? []))[0] ?? null;
+
+        if (null === $graph) {
+            return null;
         }
 
         $event = new EventValidationDTO(self::NAME);
-
-        $event->setOrganizer($organizer);
-
+        $event->setOrganizer(self::ORGANIZER);
         $lieuName = 'Espace Mendès France';
         $location = $this->postalAddressRepository->findOneBy(['name' => $lieuName]);
         $event->setLocation($location);
 
-        $title = $crawler->filter('h1.elementor-heading-title')->text();
-        $event->setTitle($title);
-        $slugger = new AsciiSlugger();
-        $slug = $slugger->slug($organizer.'-'.$title)->lower()->toString();
-        $event->setSlug($title);
+        $name = $graph['name'] ?? null;
+        if (\is_string($name)) {
+            $event->setTitle($name);
+        }
 
-        $url = $crawler->filter("meta[property='og:url']")->attr('content');
-        if ($url) {
+        $url = $graph['url'] ?? null;
+        if (\is_string($url)) {
             $event->setLink($url);
         }
 
-        $image = $crawler->filter("meta[property='og:image']")->attr('content', '');
-        if ('' !== $image) {
-            $event->setImage($image);
-        }
-
-        $description = $crawler->filter("meta[property='og:description']")->attr('content');
-        if ($description) {
+        $description = $graph['description'] ?? null;
+        if (\is_string($description)) {
             $event->setDescription($description);
         }
 
-        $dates = $crawler->filter('.ec3_schedule_date.ec3_schedule_next')->each(
-            function ($node) {
-                return $this->convertDate($node->text()); // "18 juin 2024 14 h 00 -> 16 h 00"
-            }
-        );
-
-        $eventsList = [];
-        foreach ($dates as $date) {
-            $events = clone $event;
-
-            $events->setStartAt($date);
-            $events->setSlug($events->getSlug().'-'.$date->format('Y-m-d'));
-
-            $eventsList[] = $events;
+        $thumbnailUrl = $graph['thumbnailUrl'] ?? null;
+        if (\is_string($thumbnailUrl)) {
+            $event->setImage($thumbnailUrl);
         }
 
-        return $eventsList;
+        $startAt = $crawler->filter('.elementor-element.elementor-element-c2a5359 .elementor-heading-title')->text();
+        $date = $this->convertDate($startAt);
+        if (false !== $date) {
+            $event->setStartAt($date);
+            $slugger = new AsciiSlugger();
+            $slug = $slugger->slug(self::ORGANIZER.'-'.$name.'-'.$date->format('Y-m-d'))->lower()->toString();
+            $event->setSlug($slug);
+        }
+
+        return $event;
     }
 }
